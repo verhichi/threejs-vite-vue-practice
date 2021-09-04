efc1f084f94a930d63334c9cd2a166acd8ff0176
Daichi Nishida <verhichi@hotmail.com>
2021-09-04 21:39:03 +0900

    chore: add endOfLine auto to eslintrc and run lint:fix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import 'normalize.css'
+
+import { createApp } from 'vue'
+
 import App from '@/App.vue'
 import { router } from '@/router'
-import { createApp } from 'vue'
 
 const app = createApp(App)
 
