c1ce9f6907d89e67b04bec30bcdd20dabb01ecf6
Daichi Nishida <verhichi@hotmail.com>
2021-09-08 22:29:07 +0900

    lesson: create fundamentals/primitives page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Fundamental from '@/pages/Basics/Fundamental/Fundamental.vue'
 import Responsive from '@/pages/Basics/Responsive/Responsive.vue'
+import Primitives from '@/pages/Fundamentals/Primitives/Primitives.vue'
 import Home from '@/pages/Home/Home.vue'
 
 const routes = [
   { path: '/basics/fundamental', component: Fundamental },
   { path: '/basics/responsive', component: Responsive },
+  { path: '/fundamentals/primitives', component: Primitives },
   { path: '/', component: Home },
 ]
 
